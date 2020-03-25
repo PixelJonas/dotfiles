9fc2cdce7917edbe784853766ebf84c89565e434
Jonas Janz <jonas@janz.digital>
2020-03-25 17:11:01 +0100

    add completion for zsh

---
+++ kubectl/aliases.zsh
@@ -0,0 +1,3 @@
+alias k='kubectl'
+alias kg='kubectl get'
+alias kcn='kubectl config set-context $(kubectl config current-context) --namespace '
---
+++ kubectl/completion.alias.zsh
@@ -0,0 +1,3 @@
+complete -F __start_kubectl k
+complete -F __kubectl_get_resource kg
+complete -F __kubectl_get_resource_namespace kcn

e8cdb8f133d8f8488cba300e1a89c61570e67faa
PixelJonas <jonas@janz.digital>
2020-04-07 08:52:10 +0200

    add Hyper.js configuratioadd Hyper.js configuration

---
+++ hyper/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Hyper.js https://hyper.is/
+#
+# installs the Hyper.js Terminal Emulator
+
+if test ! $(command -v hyper) && test $(command -v brew); then
+  echo "  Installing hyper for you."
+  brew cask install hyper
+fi
+
+exit 0

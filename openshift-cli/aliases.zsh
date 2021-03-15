f542899e8973c6b66523b05bada5bc3a2dfd53a3
PixelJonas <jonas@janz.digital>
2021-03-15 15:06:13 +0100

    update

--- openshift-cli/aliases.zsh
+++ openshift-cli/aliases.zsh
@@ -1,5 +1,3 @@
-alias o='oc'
-alias og='oc get'
 alias ocapprovecsr="oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{\"\n\"}}{{end}}{{end}}' | xargs oc adm certificate approve"
 function ocdecsecret(){
     oc get secret $1 -o json | jq -r '.data."'$2'"' | base64 -D -

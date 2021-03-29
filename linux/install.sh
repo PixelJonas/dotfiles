fc4ef8e2b9a716133a07bf22f4bed324e5f8519e
PixelJonas <jonas@janz.digital>
2021-03-29 15:48:34 +0200

    update installation of OpenShift CLI

--- linux/install.sh
+++ linux/install.sh
@@ -18,16 +18,9 @@ fi
 if test ! $(command -v code); then
   echo "Installing Visual Studio Code"
   curl -vsSL https://go.microsoft.com/fwlink/?LinkID=760868 > "${DOTFILE_HOME}/linux/vscode.deb"
-  sudo apt install "${DOTFILE_HOME}/linux/vscode.deb"
+  sudo apt install -y "${DOTFILE_HOME}/linux/vscode.deb"
   rm "${DOTFILE_HOME}/linux/vscode.deb"
   cp "${DOTFILE_HOME}/vscode/settings.json" "${HOME}/.config/Code/User/settings.json"
-fi
-
-if test ! $(command -v ferdi); then
-  echo "Installing Ferdi"
-  curl -vsSL https://github.com/getferdi/ferdi/releases/download/v5.5.0/ferdi_5.5.0_amd64.deb > "${DOTFILE_HOME}/linux/ferdi.deb"
-  sudo apt install "${DOTFILE_HOME}/linux/ferdi.deb"
-  rm "${DOTFILE_HOME}/linux/ferdi.deb"
 fi
 
 if test ! -d "$HOME/.local/share/fonts"; then
@@ -39,27 +32,29 @@ fi
 
 if test ! $(command -v zsh); then
   echo "Installing ZSH"
-  sudo apt --fix-broken install
-  sudo apt install zsh
+  sudo apt --fix-broken install -y
+  sudo apt install zsh -y
 fi
 
-# Configure KDE
-kwriteconfig5 --file $HOME/.config/kwinrc --group Windows --key BorderlessMaximizedWindows false
+if test $(command -v kwriteconfig5); then
+  echo "Configuring KDE Desktop"
+  # Configure KDE
+  kwriteconfig5 --file $HOME/.config/kwinrc --group Windows --key BorderlessMaximizedWindows false
 
-#set titlebar buttons
-kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key BorderSize "Normal"
-kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnLeft "MF"
-kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnRight "IAX"
-kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key CloseOnDoubleClickOnMenu "false"
-kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ShowToolTips "false"
+  #set titlebar buttons
+  kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key BorderSize "Normal"
+  kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnLeft "MF"
+  kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ButtonsOnRight "IAX"
+  kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key CloseOnDoubleClickOnMenu "false"
+  kwriteconfig5 --file $HOME/.config/kwinrc --group org.kde.kdecoration2 --key ShowToolTips "false"
 
-# keyboard settings
-kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key KeyboardRepeating "0"
-kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key NumLock "2"
-kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key RepeatDelay "300"
-kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key RepeatRate "40"
+  # keyboard settings
+  kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key KeyboardRepeating "0"
+  kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key NumLock "2"
+  kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key RepeatDelay "300"
+  kwriteconfig5 --file $HOME/.config/kcminputrc --group Keyboard --key RepeatRate "40"
 
-# disable restoring of desktop sessions
-kwriteconfig5 --file $HOME/.config/ksmserverrc --group General --key loginMode "default"
-
-# setup desktop panels
+  # disable restoring of desktop sessions
+  kwriteconfig5 --file $HOME/.config/ksmserverrc --group General --key loginMode "default"
+  # setup desktop panels
+fi

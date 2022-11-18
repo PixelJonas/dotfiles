4960a09de77a33e85b9463fc8816094113d2ee59
StinkyBenji <ether852@gmail.com>
2022-11-18 15:06:38 +0100

    add submodules: dotbot antigen

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,6 +2,7 @@
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting pj colored-man-pages ssh-agent oc you-should-use)
+
+
 
 PROJECT_PATHS=($HOME/projects /mnt/d/projects)

ef74b26b8228d38cc11369dbbba28c70c9dead0a
PixelJonas <5434875+PixelJonas@users.noreply.github.com>
2026-04-28 10:30:34 +0200

    Refactor configuration and installation scripts
    - Updated Brewfile with consistent formatting and organization of taps, brews, casks, and VSCode extensions.
    - Modified d.conf to enhance readability and maintainability of GNOME settings.
    - Improved install.sh script for better handling of Flatpak installations and dconf settings.
    - Refined bootstrap script for clearer installation process of dotfiles and dependencies.
    - Cleaned up zshrc.symlink by removing unnecessary comments and whitespace.
    - Added pdf-remove-blank.sh script to remove blank pages from PDF documents based on color density.
    
    Signed-off-by: PixelJonas <5434875+PixelJonas@users.noreply.github.com>

---
+++ bin/pdf-remove-blank.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#set -x -e -o pipefail
+set -e -o pipefail
+export LC_ALL=C
+
+#IN="$1"
+IN="$DOCUMENT_WORKING_PATH"
+
+# Check for PDF format
+TYPE=$(file -b "$IN")
+
+if [ "${TYPE%%,*}" != "PDF document" ]; then
+  >&2 echo "Skipping $IN - non PDF [$TYPE]."
+  exit 0
+fi
+
+# PDF file - proceed
+
+#PAGES=$(pdfinfo "$IN" | grep ^Pages: | tr -dc '0-9')
+PAGES=$(pdfinfo "$IN" | awk '/Pages:/ {print $2}')
+
+>&2 echo Total pages $PAGES
+
+
+# Threshold for HP scanners
+# THRESHOLD=1
+# Threshold for Canon MX925
+THRESHOLD=1  
+
+
+non_blank() {
+  for i in $(seq 1 $PAGES) ; do
+    PERCENT=$(gs -o -  -dFirstPage=${i} -dLastPage=${i} -sDEVICE=ink_cov "${IN}" | grep CMYK | nawk 'BEGIN { sum=0; } {sum += $1 + $2 + $3 + $4;} END {  printf "%.5f\n", sum } ')
+    >&2 echo -n "Color-sum in page $i is $PERCENT: "
+    if awk "BEGIN { exit !($PERCENT > $THRESHOLD) }"; then
+      echo $i
+      >&2 echo "Page added to document"
+    else
+      >&2 echo "Page removed from document"
+    fi
+  done
+}
+
+NON_BLANK=$(non_blank)
+
+if [ -n "$NON_BLANK" ]; then
+  NON_BLANK=$(echo $NON_BLANK  | tr ' ' ",")
+  qpdf "$IN" --replace-input --pages . $NON_BLANK --
+fi
